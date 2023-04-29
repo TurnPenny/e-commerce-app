a0c6bcd358149dedc88496148e6183b2d902a571
turnPenny <bohdan.miroshnik23@gmail.com>
2023-04-29 19:42:22 +0200

    data from db

--- app.js
+++ app.js
@@ -3,23 +3,50 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
-//MIDDLEWARES
-app.use(bodyParser.json());
-app.use(morgan('tiny'));
-
 require('dotenv/config');
 
 const api = process.env.API_URL;
 const port = process.env.PORT;
+//MIDDLEWARES
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
-app.get(`${api}/products`, (req, res) => {
-  const product = {
-    id: 1,
-    name: 'name',
-    image: 'url',
-  };
-  res.send(product);
+const productSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  countInStock: Number,
+});
+
+const Product = mongoose.model('Products', productSchema);
+
+app.get(`${api}/products`, async (req, res) => {
+  const productList = await Product.find();
+
+  if (!productList) {
+    res.status(500).json({ success: false });
+  }
+
+  res.send(productList);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const { name, image, countInStock } = req.body;
+  const product = new Product({
+    name,
+    image,
+    countInStock,
+  });
+  product
+    .save()
+    .then((createdProduct) => {
+      res.status(201).json(createdProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        success: false,
+      });
+    });
 });
 
 mongoose
