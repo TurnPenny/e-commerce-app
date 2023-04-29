e4f3b21000a68f0553b6b3c6c176600cbc461149
turnPenny <bohdan.miroshnik23@gmail.com>
2023-04-29 19:12:48 +0200

    mongo connect

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+//MIDDLEWARES
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+require('dotenv/config');
+
+const api = process.env.API_URL;
+const port = process.env.PORT;
+
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: 'name',
+    image: 'url',
+  };
+  res.send(product);
 });
 
-app.listen(3000, () => {
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => {
+    console.log('database is ready');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(port || 3000, () => {
+  console.log(api);
   console.log('server is running');
 });
